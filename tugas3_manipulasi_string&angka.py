a07848a472933ccbd7651eb436c595ee27a983bf
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-11-23 17:03:42 +0700

    manipulasi string & number

---
+++ tugas3_manipulasi_string&angka.py
@@ -0,0 +1,11 @@
+nama_barang ="rinso cair"
+no_barang ="0001"
+tgl_masuk ="2022-03-21"
+kode_barang_masuk ="RC0001/2022/03"
+
+data_barang_masuk ="nama barang yang masuk: {}. nomer barang yang masuk: {}. tanggal barang masuk: {}. kode barang yang masuk:{}."
+print(data_barang_masuk.format(nama_barang,no_barang,tgl_masuk,kode_barang_masuk))
+
+data_barang_masuk ="kode barang yang masuk: {3}. tanggal barang masuk: {2}. nomer barang yang masuk: {1}. nama barang yang masuk: {0}"
+print(data_barang_masuk.format(nama_barang,no_barang,tgl_masuk,kode_barang_masuk))
+
